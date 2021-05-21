a9dae7a8dd83de406faf490235da7d97eaf099db
chandrikachenna <chennachandrika@gmail.com>
2021-05-21 06:40:36 +0000

    code files

---
+++ index.js
@@ -0,0 +1,8 @@
+const { addDays, format } = require("date-fns");
+const add_days_to_date = function (given_days) {
+  const result = addDays(new Date(2020, 7, 22), given_days);
+  //   return format(result, "dd-MM-yyyy");
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
+};
+console.log(add_days_to_date(3));
+module.exports = add_days_to_date;
